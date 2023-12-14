71e5a7fc8676b5aae83cc7bcde9445215898bee8
adam1279 <adamdamgolan@gmail.com>
2023-12-14 01:43:45 +0100

    init

---
+++ id-quiz/script.js
@@ -0,0 +1,15 @@
+let result = 0;
+const qs = [
+    {
+        str: "På en skala fra 1 til 10, hvordan vurderer du dine kompetencer i "
+    }
+];
+const subjects = [
+    ["CAD", ]
+];
+for ()
+class Question {
+    constructor({str, lr, weight}) {
+        this.lr = lr;
+    }
+}
